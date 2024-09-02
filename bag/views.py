cb992c80e78e75eee4d6aef852128fd6063e7695
Kamen Kirov <kamen@bgtown.com>
2024-09-02 13:00:15 +0000

    Added remove functionality.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -18,34 +18,67 @@ def add_to_bag(request, item_id):
     bag = request.session.get('bag', {})
 
     if size:
-        # Handle case with product size
-        if item_id in bag:
-            # Ensure bag[item_id] is a dictionary
-            if isinstance(bag[item_id], int):
-                # Convert to dictionary format if currently an integer
-                bag[item_id] = {'items_by_size': {}, 'quantity': bag[item_id]}
-
-            # Now handle the items by size
-            if size in bag[item_id].get('items_by_size', {}):
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
             else:
-                bag[item_id].setdefault('items_by_size', {})[size] = quantity
+                bag[item_id]['items_by_size'][size] = quantity
         else:
-            # Initialize with size
             bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        # Handle case without product size
-        if item_id in bag:
-            # Ensure bag[item_id] is treated correctly
-            if isinstance(bag[item_id], dict):
-                # If it's already a dictionary, add to the quantity
-                bag[item_id]['quantity'] = bag[item_id].get('quantity', 0) + quantity
-            else:
-                # If it's an integer, just add the quantity
-                bag[item_id] += quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
         else:
-            # Initialize without size
             bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+    
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
