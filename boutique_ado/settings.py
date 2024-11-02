66b23a305f5accc4f1c723c189046849a75c349e
Kamen Kirov <kamen@bgtown.com>
2024-11-02 08:25:17 +0000

    login page edited.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [  '8000-kamenco-ado-imed8ubomxr.ws.codeinstitute-ide.net']
 
