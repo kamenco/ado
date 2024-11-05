53ce521ac36e7ebe5cfcd875290234d76e6125cd
Kamen Kirov <kamen@bgtown.com>
2024-11-05 10:31:17 +0000

    Added delete functionality.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [  '8000-kamenco-ado-imed8ubomxr.ws.codeinstitute-ide.net']
 
