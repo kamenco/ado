4df90c541f99662998d6b349d025466c3b910ee1
Kamen Kirov <kamen@bgtown.com>
2024-11-09 22:21:41 +0000

    Added cache control.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -206,6 +206,13 @@ AWS_DEFAULT_ACL = None
 
 
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config for the AWS S3
     AWS_STORAGE_BUCKET_NAME = 'apparelkamen'
     AWS_S3_REGION_NAME = 'eu-north-1'
